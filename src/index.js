6f70d4ae1fdea6e21eed2be73b4ef81c6c2c1367
Billy Cole <cole.billys@gmail.com>
2020-12-02 15:55:38 -0800

    modify index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 ReactDOM.render(
-  <div>App</div>,
-  document.getElementById('root')
+  (
+    <Fragment>
+      <div>
+        1) change the name of the
+        <span>storybook</span>
+        folder to
+        <span>.storybook</span>
+      </div>
+      <div>
+        2) run
+        <span>yarn storybook</span>
+        in the console to load storybook
+      </div>
+    </Fragment>
+  ),
+  document.getElementById('root'),
 );
